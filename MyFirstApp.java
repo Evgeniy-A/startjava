3416f888f6b211aa4921c67694428ceba753969c
Evgeniy-A <Antonov.174.rus@mail.ru>
2025-03-01 21:39:38 +0500

    Изменил вывод текста, отображаемого в консоль

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 public class MyFirstApp {
     public static void main(String[] args) {
-        System.out.println("Write once, run anywhere тест");
-        Scanner scanner = new Scanner(System.in, "cp866");
-        System.out.println("Введите имя");
-        System.out.println(scanner.nextLine());
+        System.out.print("Написано однажды, ");
+        System.out.println("работает везде!");
     }
 }
