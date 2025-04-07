45a3b2fe78ded1e646a94748b63794958c8da947
Евгений <Antonov.174.rus@mail.ru>
2025-04-07 17:50:25 +0500

    Скорректировал код

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -31,12 +31,12 @@ public class IfElseStatementTheme {
         }
 
         System.out.println("\n2. ПОИСК БОЛЬШЕГО ЧИСЛА");
-        int firstNumber = 1;
-        int secondNumber = 2;
+        int firstNumber = 123;
+        int secondNumber = 223;
         if (firstNumber > secondNumber) {
-            System.out.printf("Число %d больше числа %d", firstNumber, secondNumber);
+            System.out.printf("Число %d больше числа %d%n", firstNumber, secondNumber);
         } else if (firstNumber < secondNumber) {
-            System.out.printf("Число %d больше числа %d", secondNumber, firstNumber);
+            System.out.printf("Число %d больше числа %d%n", secondNumber, firstNumber);
         } else {
             System.out.println("Числа равны");
         }
@@ -59,79 +59,72 @@ public class IfElseStatementTheme {
         }
 
         System.out.println("\n4. ПОИСК ОДИНАКОВЫХ ЦИФР В ЧИСЛАХ");
-        int firstValue = 123;
-        int secondValue = 223;
-        if (firstValue / 100 != secondValue / 100 &&
-                (firstValue / 10) % 10 != (secondValue / 10) % 10 &&
-                firstValue % 10 != secondValue % 10) {
+        boolean isHundredsEqual = firstNumber / 100 == secondNumber / 100;
+        boolean isTensEqual = (firstNumber / 10) % 10 == (secondNumber / 10) % 10;
+        boolean isUnitsEqual = firstNumber % 10 == secondNumber % 10;
+        if (!isHundredsEqual && !isTensEqual && !isUnitsEqual) {
             System.out.println("Равных цифр в числах нет");
         } else {
-            System.out.println(firstValue + " и " + secondValue);
-            if (firstValue / 100 == secondValue / 100) {
-                System.out.printf("Сотни равны между собой - %d\n", firstValue / 100);
+            System.out.println(firstNumber + " и " + secondNumber);
+            if (isHundredsEqual) {
+                System.out.printf("Сотни равны между собой - %d\n", firstNumber / 100);
             }
-            if ((firstValue / 10) % 10 == (secondValue / 10) % 10) {
-                System.out.printf("Десятки равны между собой - %d\n", (firstValue / 10) % 10);
+            if (isTensEqual) {
+                System.out.printf("Десятки равны между собой - %d\n", (firstNumber / 10) % 10);
             }
-            if (firstValue % 10 == secondValue % 10) {
-                System.out.printf("Единицы равны между собой - %d\n", firstValue % 10);
+            if (isUnitsEqual) {
+                System.out.printf("Единицы равны между собой - %d\n", firstNumber % 10);
             }
         }
 
         System.out.println("\n5. ОПРЕДЕЛЕНИЕ СИМВОЛА ПО ЕГО КОДУ");
-        char valueChar = '\u0057';
-        if (Character.isLetter(valueChar)) {
-            if (Character.isUpperCase(valueChar)) {
-                System.out.printf("'%c' - большая буква\n", valueChar);
-            } else {
-                System.out.printf("'%c' - маленькая буква\n", valueChar);
-            }
-        } else if (Character.isDigit(valueChar)) {
-            System.out.printf("'%c' - цифра\n", valueChar);
+        char unknownChar = '\u0057';
+        if (Character.isUpperCase(unknownChar)) {
+            System.out.printf("'%c' - большая буква\n", unknownChar);
+        } else if (Character.isLowerCase(unknownChar)) {
+            System.out.printf("'%c' - маленькая буква\n", unknownChar);
+        } else if (Character.isDigit(unknownChar)) {
+            System.out.printf("'%c' - цифра\n", unknownChar);
         } else {
-            System.out.printf("'%c' - ни буква и ни цифра\n", valueChar);
+            System.out.printf("'%c' - ни буква и ни цифра\n", unknownChar);
         }
 
         System.out.println("\n6. ПОДСЧЕТ НАЧИСЛЕННЫХ БАНКОМ %");
-        double amount = 321123.59;
-        double interestAmount;
+        float amount = 321123.59f;
+        float interestAmount;
         if (amount < 100000) {
-            interestAmount = amount * 0.05;
+            interestAmount = amount * 0.05f;
         } else if (amount <= 300000) {
-            interestAmount = amount * 0.07;
+            interestAmount = amount * 0.07f;
         } else {
-            interestAmount = amount * 0.1;
+            interestAmount = amount * 0.1f;
         }
         System.out.printf("Сумма вклада - %,.2f%nCумма начисленного %% - %,.2f%n" +
-                "Итоговая сумму с %% -%,.2f%n", amount, interestAmount, amount + interestAmount);
+                "Итоговая сумма с %% - %,.2f%n", amount, interestAmount, amount + interestAmount);
 
         System.out.println("\n7. ОПРЕДЕЛЕНИЕ ОЦЕНКИ ПО ПРЕДМЕТАМ");
         int historyPercentage = 59;
-        int historyScore;
-        int programmingPercentage = 92;
-        int programmingScore;
+        int historyScore = 5;
         if (historyPercentage <= 60) {
             historyScore = 2;
         } else if (historyPercentage <= 73) {
             historyScore = 3;
         } else if (historyPercentage <= 91) {
             historyScore = 4;
-        } else {
-            historyScore = 5;
         }
+        int programmingPercentage = 92;
+        int programmingScore = 5;
         if (programmingPercentage <= 60) {
             programmingScore = 2;
         } else if (programmingPercentage <= 73) {
             programmingScore = 3;
         } else if (programmingPercentage <= 91) {
             programmingScore = 4;
-        } else {
-            programmingScore = 5;
         }
         System.out.printf("История - %d%nПрограммирование - %d%n", historyScore, programmingScore);
-        System.out.printf("Средний балл оценок по предметам - %f%n",
+        System.out.printf("Средний балл оценок по предметам - %,.1f%n",
                 (double) (historyScore + programmingScore) / 2);
-        System.out.printf("средний %% по предметам - %f%n",
+        System.out.printf("Средний %% по предметам - %,.1f%n",
                 (double) (historyPercentage + programmingPercentage) / 2);
 
         System.out.println("\n8. РАСЧЕТ ГОДОВОЙ ПРИБЫЛИ");
@@ -139,34 +132,36 @@ public class IfElseStatementTheme {
         double rent = 5123.018;
         double costOfProduction = 9001.729;
         double annualProfit = 12 * (sales - rent - costOfProduction);
+        if (annualProfit == 0) {
+            System.out.println("Прибыль за год 0 руб.");
+        }
         if (annualProfit > 0) {
             System.out.printf("Прибыль за год: + %,.2f руб.%n", annualProfit);
-        } else if (annualProfit < 0) {
-            System.out.printf("Прибыль за год: %,.2f руб.%n", annualProfit);
         } else {
-            System.out.println("Прибыль за год 0 руб.");
+            System.out.printf("Прибыль за год: %,.2f руб.%n", annualProfit);
         }
 
         System.out.println("\n9.*РАСЧЕТ ГОДОВОЙ ПРИБЫЛИ");
         var salesBigD = new BigDecimal("13025.233");
         var rentBigD = new BigDecimal("5123.018");
         var costOfProductionBigD = new BigDecimal("9001.729");
-        var annualProfitBigD = new BigDecimal(12).multiply(salesBigD.subtract(rentBigD)
+        var annualProfitBigD = BigDecimal.valueOf(12).multiply(salesBigD.subtract(rentBigD)
                 .subtract(costOfProductionBigD));
+        if (annualProfitBigD.compareTo(BigDecimal.ZERO) == 0) {
+            System.out.println("Прибыль за год 0 руб.");
+        }
         if (annualProfitBigD.compareTo(BigDecimal.ZERO) > 0) {
             System.out.printf("Прибыль за год: + %,.2f руб.%n", annualProfit);
-        } else if (annualProfitBigD.compareTo(BigDecimal.ZERO) < 0) {
-            System.out.printf("Прибыль за год: %,.2f руб.%n", annualProfit);
         } else {
-            System.out.println("Прибыль за год 0 руб.");
+            System.out.printf("Прибыль за год: %,.2f руб.%n", annualProfit);
         }
 
         System.out.println("\n10.*ПОДСЧЕТ НАЧИСЛЕННЫХ БАНКОМ %");
         var amountBigD = new BigDecimal("321123.59");
         var interestAmountBigD = BigDecimal.ZERO;
-        if (amountBigD.compareTo(new BigDecimal("100000")) < 0) {
+        if (amountBigD.compareTo(BigDecimal.valueOf(100000)) < 0) {
             interestAmountBigD = amountBigD.multiply(new BigDecimal("0.05"));
-        } else if (amountBigD.compareTo(new BigDecimal("300000")) <= 0) {
+        } else if (amountBigD.compareTo(BigDecimal.valueOf(300000)) <= 0) {
             interestAmountBigD = amountBigD.multiply(new BigDecimal("0.07"));
         } else {
             interestAmountBigD = amountBigD.multiply(new BigDecimal("0.1"));
