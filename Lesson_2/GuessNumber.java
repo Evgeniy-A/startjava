9e1fb5da256f5f5345b5941462c5a1a8b2c892ed
Евгений <Antonov.174.rus@mail.ru>
2025-05-01 16:48:13 +0500

    внес исправления

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -2,8 +2,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
-    Player player1;
-    Player player2;
+    private Player player1;
+    private Player player2;
 
     public GuessNumber (Player player1, Player player2){
         this.player1 = player1;
