a07b3b37e9695329e64d7ef7e24a3e8ff70cb274
Евгений <Antonov.174.rus@mail.ru>
2025-05-01 15:59:33 +0500

    внес исправления

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -5,7 +5,7 @@ public class GuessNumber {
     Player player1;
     Player player2;
 
-    public GuessNumber (Player player1, Player player2) {
+    public GuessNumber (Player player1, Player player2){
         this.player1 = player1;
         this.player2 = player2;
     }
