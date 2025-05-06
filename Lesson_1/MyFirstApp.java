4aea2fa8d1c67b72306232cc56f2cb6411294893
Евгений <Antonov.174.rus@mail.ru>
2025-05-06 17:52:24 +0500

    Удалены старые файлы из индекса

--- Lesson_1/MyFirstApp.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class MyFirstApp {
-    public static void main(String[] args) {
-        System.out.print("Написано однажды, ");
-        System.out.println("работает везде!");
-    }
-}
--- Lesson_2/Util.java
+++
@@ -1,5 +0,0 @@
-public class Util {
-    public static String getCurrentMethodName() {
-        return Thread.currentThread().getStackTrace()[2].getMethodName();
-    }
-}
