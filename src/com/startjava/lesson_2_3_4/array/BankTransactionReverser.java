c9a1f1b6e94438fda98105f68700d5cd1a843417
Евгений <Antonov.174.rus@mail.ru>
2025-07-23 19:00:00 +0500

    Улучшена читаемость кода: 1. Массивы вынесены в переменные в `main` 2. Метод `printResult` переименован в `printTransactionsWithReversed` 3. Оптимизирована работа с `length` в методе `reverse`

--- src/com/startjava/lesson_2_3_4/array/BankTransactionReverser.java
+++ src/com/startjava/lesson_2_3_4/array/BankTransactionReverser.java
@@ -4,11 +4,16 @@ import java.util.Arrays;
 
 public class BankTransactionReverser {
     public static void main(String[] args) {
-        printResult(new int[0], reverse(new int[0]));
-        printResult(null, reverse(null));
-        printResult(new int[]{5}, reverse(new int[]{5}));
-        printResult(new int[]{6, 8, 9, 1}, reverse(new int[]{6, 8, 9, 1}));
-        printResult(new int[]{13, 8, 5, 3, 2, 1, 1}, reverse(new int[]{13, 8, 5, 3, 2, 1, 1}));
+        int[] firstTr = new int[0];
+        printTransactionsWithReversed(firstTr, reverse(firstTr));
+        int[] secondTr = null;
+        printTransactionsWithReversed(secondTr, reverse(secondTr));
+        int[] thirdTr = {5};
+        printTransactionsWithReversed(thirdTr, reverse(thirdTr));
+        int[] fourthTr = {6, 8, 9, 1};
+        printTransactionsWithReversed(fourthTr, reverse(fourthTr));
+        int[] fifthTr = {13, 8, 5, 3, 2, 1, 1};
+        printTransactionsWithReversed(fifthTr, reverse(fifthTr));
     }
 
     private static int[] reverse(int[] transactions) {
@@ -16,19 +21,19 @@ public class BankTransactionReverser {
             System.out.println("Ошибка в данных, информация о транзакциях отсутствует");
             return null;
         }
-        if (transactions.length == 0) {
+        int length = transactions.length;
+        if (length == 0) {
             System.out.println("Пользователь еще не совершал транзакций");
             return new int[0];
         }
-        int length = transactions.length - 1;
-        int[] reversed = new int[transactions.length];
+        int[] reversed = new int[length];
         for (int tr : transactions) {
-            reversed[length--] = tr;
+            reversed[--length] = tr;
         }
         return reversed;
     }
 
-    private static void printResult(int[] transactions, int[] reversed) {
+    private static void printTransactionsWithReversed(int[] transactions, int[] reversed) {
         if (reversed != null && reversed.length != 0) {
             System.out.printf("Исходные транзакции: %s%n", Arrays.toString(transactions));
             System.out.printf(" В обратном порядке: %s%n", Arrays.toString(reversed));
