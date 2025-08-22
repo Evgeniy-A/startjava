8b516c45cae76602ac084b9408d71c881292f9ac
Евгений <Antonov.174.rus@mail.ru>
2025-08-22 17:40:23 +0500

    Поправил замечания

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,7 +1,5 @@
 package com.startjava.lesson_2_3_4.calculator;
 
-import java.text.DecimalFormat;
-
 public class Calculator {
     public double calculate(String expression) {
         String[] splitExpression = expression.replaceAll("\\s+", "")
@@ -9,37 +7,26 @@ public class Calculator {
         int firstNumber = Integer.parseInt(splitExpression[0]);
         int secondNumber = Integer.parseInt(splitExpression[2]);
         String operator = splitExpression[1];
-        switch (operator) {
-            case "+" -> {
-                return firstNumber + secondNumber;
-            }
-            case "-" -> {
-                return firstNumber - secondNumber;
-            }
-            case "*" -> {
-                return firstNumber * secondNumber;
-            }
-            case "/" -> {
+        return switch (operator) {
+            case "+" -> firstNumber + secondNumber;
+            case "-" -> firstNumber - secondNumber;
+            case "*" -> firstNumber * secondNumber;
+            case "/", "%" -> {
                 if (secondNumber == 0) {
                     System.out.println("Ошибка: деление на 0!");
-                    return Double.NaN;
+                    yield Double.NaN;
                 }
-                return (double) firstNumber / secondNumber;
-            }
-            case "^" -> {
-                return Math.pow(firstNumber, secondNumber);
-            }
-            case "%" -> {
-                if (secondNumber == 0) {
-                    System.out.println("Ошибка: попытка деления на 0");
-                    return Double.NaN;
+                if (operator.equals("/")) {
+                    yield (double) firstNumber / secondNumber;
+                } else {
+                    yield Math.IEEEremainder(firstNumber, secondNumber);
                 }
-                return Math.IEEEremainder(firstNumber, secondNumber);
             }
+            case "^" -> Math.pow(firstNumber, secondNumber);
             default -> {
                 System.out.printf("Ошибка: операция %s не поддерживается%n", operator);
-                return Double.NaN;
+                yield Double.NaN;
             }
-        }
+        };
     }
 }
